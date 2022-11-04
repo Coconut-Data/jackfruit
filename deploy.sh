83605ccc3533ac4bd072f16939d2a7f01a6a0eff
jongoz <jongo@mohz.go.tz>
2022-11-04 11:37:45 +0300

    adding karafuu

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 echo "Packaging source code together into bundle.js"
 npx browserify -t coffeeify --extension='.coffee' Jackfruit.coffee -o bundle.js
 echo "Compressing bundle.js"
-cat bundle.js | npx terser | sponge bundle.js
+cat bundle.js | npx terser | npx sponge bundle.js
 echo "Copying results to server"
-rsync --progress --verbose --copy-links --exclude=node_modules --recursive ./ jackfruit.cococloud.co:/var/www/jackfruit.cococloud.co/
+rsync --progress --verbose --copy-links --exclude=node_modules --recursive ./ karafuu@coconut.mohz.go.tz:/var/www/jackfruit/
