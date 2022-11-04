c1f21b33c1b08dfde5ae7b803bdf8281925eba6c
Mike McKay <mikeymckay@gmail.com>
2022-11-04 10:48:40 +0300

    Forcing an old version of aws packages

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Packaging source code together into bundle.js"
 npx browserify -t coffeeify --extension='.coffee' Jackfruit.coffee -o bundle.js
+echo "Compressing bundle.js"
 cat bundle.js | npx terser | sponge bundle.js
+echo "Copying results to server"
 rsync --progress --verbose --copy-links --exclude=node_modules --recursive ./ jackfruit.cococloud.co:/var/www/jackfruit.cococloud.co/
