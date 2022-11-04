a9dceea54b6967021ec26f734d0eec08b74638e8
Mike McKay <mikeymckay@gmail.com>
2022-11-04 11:57:40 +0300

    Updated script to ignore .git directory

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ npx browserify -t coffeeify --extension='.coffee' Jackfruit.coffee -o bundle.js
 echo "Compressing bundle.js"
 cat bundle.js | npx terser | npx sponge bundle.js
 echo "Copying results to server"
-rsync --progress --verbose --copy-links --exclude=node_modules --recursive ./ karafuu@coconut.mohz.go.tz:/var/www/jackfruit/
+rsync --progress --verbose --copy-links --exclude=.git --exclude=node_modules --recursive ./ karafuu@coconut.mohz.go.tz:/var/www/jackfruit/
